dd1c7bd685ec6a9f42ce4aa5d76e9e1251b0dce4
Marcella Yanes <102261833+yanesmarcella@users.noreply.github.com>
2022-10-06 22:57:25 -0300

    Add files via upload

---
+++ API/Pro4jobs/src/Login_candidato.java
@@ -0,0 +1,25 @@
+
+public class Login_candidato {
+
+    private String Cpf;
+    private String Senha;
+
+    public String getCpf() {
+        return Cpf;
+    }
+
+    public void setCpf(String Cpf) {
+        this.Cpf = Cpf;
+    }
+
+    public String getSenha() {
+        return Senha;
+    }
+
+    public void setSenha(String Senha) {
+        this.Senha = Senha;
+    }
+
+
+    
+}
