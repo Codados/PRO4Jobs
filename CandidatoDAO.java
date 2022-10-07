f697e0b4ea51a7a2c49b0b8856c30369923291c0
Marcella Yanes <102261833+yanesmarcella@users.noreply.github.com>
2022-10-06 22:46:09 -0300

    Update CandidatoDAO.java

--- CandidatoDAO.java
+++ CandidatoDAO.java
@@ -59,6 +59,19 @@ public class CandidatoDAO {
         } catch(Exception erro){
             JOptionPane.showMessageDialog(null, "CandidatoDAO" + erro);
         }
+             public void LoginCandidato(Candidato objcandidato){
+        String sql = "select * from candidato where cpf = ? and senha = *";
+        conn = new Dao.conectaBD();
+        
+        try {
+            pstm = conn.PrepareStatement(sql); 
+            pstm.setString(0, objcandidato.getCpf());
+            pstm.setString(1,objcandidato.getSenha());
+            pstm.execute();
+            pstm.close();
+        } catch(Exception erro){
+            JOptionPane.showMessageDialog(null, "CandidatoDAO" + erro);
+        }
         public static void main(String[] args) {
         // TODO code application logic here
     }
