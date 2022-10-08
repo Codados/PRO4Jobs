5ccc15fa2f6124c5bbf9f0268561febe3c5093d9
Lucas Emanoel <102261954+lucasetdasilva@users.noreply.github.com>
2022-10-08 19:00:20 -0300

    Create Experiencia_Academica.java

---
+++ API Java/src/api/DTO/Experiencia_Academica.java
@@ -0,0 +1,30 @@
+package api.conexao.DTO;
+
+/**
+ *
+ * @author Lucas Emanoel
+ */
+public class Experiencia_Academica {
+    private String Descricao;
+
+    /**
+     * @return the Descricao
+     */
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    /**
+     * @param Descricao the Descricao to set
+     */
+    public void setDescricao(String Descricao) {
+        this.Descricao = Descricao;
+    }
+    
+    
+    public static void main(String[] args) {
+     
+        
+    }
+    
+}
