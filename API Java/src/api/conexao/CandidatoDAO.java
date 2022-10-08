733c050c4baec3e0bd86097ceb33216d98b3a94b
Lucas Emanoel <102261954+lucasetdasilva@users.noreply.github.com>
2022-10-08 19:35:42 -0300

    Update CandidatoDAO.java

--- API Java/src/api/conexao/CandidatoDAO.java
+++ API Java/src/api/conexao/CandidatoDAO.java
@@ -7,36 +7,54 @@ package api.conexao;
 import api.conexao.DTO.Candidato;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Lucas Emanoel
- */
 public class CandidatoDAO {
 
     Connection conn;
     PreparedStatement pstm;
+    
+    public ResultSet LoginCandidato(Login_candidato objcandidato){
+        String sql = "select * from candidato where cpf = ? and senha = ?";
+        conn = new ConexaoDao.conectaBD();
+        
+        try {
+            pstm = conn.PrepareStatement(sql); 
+            pstm.setString(0, objcandidato.getCpf());
+            pstm.setString(1,objcandidato.getSenha());
+            ResultSet rs = pstm.executeQuery();
+            return rs;
+        } catch(Exception erro){
+            JOptionPane.showMessageDialog(null, "CandidatoDAO" + erro);
+            return null;
+        }
+        
+    }
+    
+   
+     public void cadastrarCandidato(Candidato objcandidato) {
 
-    public void cadastrarCandidato(Candidato objcandidato) {
-
-        String sql = "insert into candidato (CPF, Nome_Completo, Data_Nascimento, Email, Telefone, Endereco, Senha) values (?,?,?,?,?,?,?)";
-        conn = new Dao().conectaBD();
+        String sql = "insert into candidato (cpf, nome_completo, data_nascimento, email, telefone, endereco, pretencao_salarial, senha) values (?,?,?,?,?,?,?,?)";
+        conn = new ConexaoDAO().conectaBD();
 
         try {
             
             pstm = conn.prepareStatement(sql);
             
-            pstm.setInt(0, objcandidato.getCPF());
-            pstm.setString(1,objcandidato.getNome_Completo());
-            pstm.setString(2, objcandidato.getData_Nascimento());
-            pstm.setString(3, objcandidato.getEmail());
-            pstm.setInt(4, objcandidato.getTelefone());
-            pstm.setString(5, objcandidato.getEndereco());
-            pstm.setString(6, objcandidato.getSenha());
+            pstm.setString(1, objcandidato.getCPF());
+            pstm.setString(2,objcandidato.getNome_Completo());
+            pstm.setString(3, objcandidato.getData_Nascimento());
+            pstm.setString(4, objcandidato.getEmail());
+            pstm.setLong(5, objcandidato.getTelefone());
+            pstm.setString(6, objcandidato.getEndereco());
+            pstm.setDouble(7, objcandidato.getSalario());
+            pstm.setString(8, objcandidato.getSenha());
             
             pstm.execute();
             pstm.close();
+            
+            JOptionPane.showMessageDialog(null, "Os dados foram salvos !");
 
         } catch (Exception erro) {
             JOptionPane.showMessageDialog(null, "CandidatoDAO" + erro);
@@ -44,9 +62,42 @@ public class CandidatoDAO {
 
     }
     
-    public static void main(String[] args) {
-     
+    public void cadastrarexperiencias(Experiencia_Academica objexacd, Candidato objcandidato, Experiecia_Profissional objexprof){
+        
+        String sql = "insert into formacao_academica(descricao,fk_cpf) values (?,?)";
+        
+        String sql2 = "insert into experiencia_profissional(descricao,fk_cpf) values (?,?)";
+        
+        conn = new ConexaoDAO().conectaBD();
+        
+        try{
+            pstm = conn.prepareStatement(sql);
+            
+            pstm.setString(1, objexacd.getDescricao());
+            pstm.setString(2, objcandidato.getCPF());
+            pstm.execute();
+            pstm.close();
+            
+            pstm = conn.prepareStatement(sql2);
+            
+            pstm.setString(1, objexprof.getDescricao());
+            pstm.setString(2,objcandidato.getCPF());
+            pstm.execute();
+            pstm.close();
+            
+            
+        } catch(Exception erro){
+            JOptionPane.showMessageDialog(null, "CandidatoDAO" + erro);
+            
+        }
         
     }
-
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    
+    
 }
